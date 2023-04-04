a972682b158477b1a5ffdedc6b97b158fe5168a2
Chloeminn <lovisz822@gmail.com>
2023-04-04 17:08:10 +0900

    수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 // path: NodeJS에서 파일 및 디렉토리 경로 작업을 위한 전역 모듈
-const path = require('path')
-const HtmlPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  // 파일을 읽어들이기 시작하는 진입점 설정
-  entry: './js/main.js',
+  // 파일을 읽어들이기 시작하는 진입점 설정 - webpack의 경우 html이 아닌 js파일을 진입점으로 사용
+  // parcel index.html
+  entry: "./js/main.js",
 
   // 결과물(번들)을 반환하는 설정
   output: {
-    // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: 'main.js',
-    clean: true
+    // 주석은 기본값!, `__dirname`은 현재 파일의 위치(경로)를 알려주는 NodeJS 전역 변수
+    // path: path.resolve(__dirname, "dist"),
+    // filename: "main.js",
+    //clean 옵션의 경우 새롭게 build를 하였을 때 기존 필요하지 않은 내용 제거
+    clean: true,
   },
 
   // 모듈 처리 방식을 설정
@@ -22,38 +24,34 @@ module.exports = {
         test: /\.s?css$/,
         use: [
           // 순서 중요!
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ]
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/, // 제외할 경로
-        use: [
-          'babel-loader'
-        ]
-      }
-    ]
+        use: ["babel-loader"],
+      },
+    ],
   },
 
   // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'static' }
-      ]
-    })
+      patterns: [{ from: "static" }],
+    }),
   ],
 
   // 개발 서버 옵션
   devServer: {
-    host: 'localhost',
+    host: "localhost",
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+};
